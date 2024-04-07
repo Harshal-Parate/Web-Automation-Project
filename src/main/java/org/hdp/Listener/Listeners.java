3217b393a4e57e294d11d5d6f10391ab57a82bf4
Harshal-Parate <Harshalparate1@gmail.com>
2024-04-07 18:19:32 +0530

    Listeners

--- src/main/java/org/hdp/Listener/Listeners.java
+++ src/main/java/org/hdp/Listener/Listeners.java
@@ -1,23 +1,28 @@
 package org.hdp.Listener;
 
+import io.qameta.allure.Allure;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class Listeners implements ITestListener {
 
-    @Override
-    public void onTestFailure(ITestResult result) {
-        ITestListener.super.onTestFailure(result);
-    }
-
-    @Override
-    public void onTestSkipped(ITestResult result) {
-        ITestListener.super.onTestSkipped(result);
-    }
+    Logger logger = LogManager.getLogger(Listeners.class);
 
     @Override
     public void onFinish(ITestContext context) {
         ITestListener.super.onFinish(context);
+    }
+
+    public void onTestFailure (ITestResult result) {
+        logger.error("Failure of test"+ result.getName(), result.getThrowable());
+        Allure.step(result.getName());
+    }
+
+    public void onTestSkipped (ITestResult result) {
+        logger.error("Skip of test"+ result.getName(), result.getThrowable());
+        Allure.step(result.getName());
     }
 }
